bb096a1484cf1755b58d9b1bd6797faeb2c33b9d
FinFin <FinFin1351@gmail.com>
2025-07-30 19:13:46 +0900

    fix: add conditional check for DATABASE_URL in entrypoint.sh to prevent overwriting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
-POSTGRES_PW=$(echo "$POSTGRES_PASSWORD" | jq -r .password)
-export DATABASE_URL="postgres://mosavi_admin:${POSTGRES_PW}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=require&timezone=Asia/Tokyo&search_path=relay"
+# if DATABASE_URL is set, do nothing, otherwise construct it from environment variables
+if [ -n "$DATABASE_URL" ]; then
+    echo "DATABASE_URL is already set, skipping construction."
+else
+    POSTGRES_PW=$(echo "$POSTGRES_PASSWORD" | jq -r .password)
+    export DATABASE_URL="postgres://mosavi_admin:${POSTGRES_PW}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=require&timezone=Asia/Tokyo&search_path=relay"
+    export RO_DATABASE_URL="postgres://mosavi_admin:${POSTGRES_PW}@${POSTGRES_HOST}:${POSTGRES_PORT}/${RO_POSTGRES_DB}?sslmode=require&timezone=Asia/Tokyo&search_path=relay"
+fi
+
 exec /go/bin/nostr-relay
